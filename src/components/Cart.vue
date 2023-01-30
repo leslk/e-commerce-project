471531a0e248ff56921805c37bdd30280f97e51e
Leslie El kattoussi <leslie@MacBook-Air-de-Leslie.local>
2023-01-30 15:55:31 +0100

    Create productsList and Cart components and test routes

---
+++ src/components/Cart.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { useCartStore } from '../stores/cart';
+
+    const cartStore = useCartStore();
+</script>
+
+<template>
+    <div>
+        <h1>Panier</h1>
+        <p>{{cartStore.totalQuantity}} articles</p>
+        <p>Prix total : {{cartStore.totalPrice}} €</p>
+    </div>
+</template>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { useProductsStore } from '../stores/products';
+    import { useCartStore } from '../stores/cart';
+
+    const cartStore = useCartStore();
+    const productsStore = useProductsStore();
+</script>
+
+<template>
+    <section>
+        <article v-for="product in productsStore.setProductsPriceInEuro" :key="product.productId">
+            <h1>{{product.productName}}</h1>
+            <div>
+                <img :src="product.productPicture" :alt="product.productName"/>
+            </div>
+            <p>{{product.productPrice + ' ' + '€'}}</p>
+            <button @click="addToCart(product); cartStore.totalCartPrice(); cartStore.countCartItems();">Ajouter au panier</button>
+        </article>
+    </section>
+</template>
